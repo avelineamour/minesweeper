bd1029703c451b8212a3e07f3ff53f830094b1b3
Razelle McCarrick <razelle@Razelles-MBP.fios-router.home>
2017-10-30 22:28:05 -0400

    Testing .gitignore

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -10,4 +10,4 @@ const bombLine = ' |B| ';
 console.log('This is what a board with a guess and a bomb on it would look like:');
 console.log(`${guessLine}`);
 console.log(`${bombLine}`);
-console.log(`${blankLine}`);
+console.log(`${blankLine}`); 
